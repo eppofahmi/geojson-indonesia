6d959698be340eba3b9f955f0d1eb4672f3a84f7
mymac <mymac@mymacs-MacBook-Pro.local>
2021-09-09 07:11:49 +0700

    add tes map dapil dusun desa

--- r-script/01.test-maps.R
+++ r-script/01.test-maps.R
@@ -6,7 +6,6 @@ library(rgdal)
 library(rmapshaper)
 
 # Province ----
-## Geojson ----
 province = readOGR("provinsi/all_maps_state_indo.geojson")
 
 ## Leaflet
@@ -23,8 +22,7 @@ leaflet(province) %>%
             opacity = 0.5, na.label = "")
 
 # Cities ----
-## Geojson ----
-cities = readOGR("kota/all_maps_city_indo.geojson")
+cities = readOGR("kota/all_kabkota_ind.geojson")
 
 ## Leaflet
 pal = colorNumeric("viridis", cities$sample_value)
@@ -35,5 +33,36 @@ leaflet(cities) %>%
               fillColor = ~pal(sample_value),
               label = ~paste0(name, ": ", prettyNum(sample_value, ","))) %>%
   addLegend("topright", pal = pal, values = ~sample_value,
+            title = "Populasi (Ribu Jiwa)", 
+            opacity = 0.5, na.label = "")
+
+
+# Dapil -----
+dapil = readOGR("dapil/all_maps_dapil.geojson")
+
+## Leaflet
+pal = colorNumeric("viridis", dapil$sample_value)
+
+leaflet(dapil) %>%
+  addTiles() %>%
+  addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.7,
+              fillColor = ~pal(sample_value),
+              label = ~paste0(dapil, ": ", prettyNum(sample_value, ","))) %>%
+  addLegend("topright", pal = pal, values = ~sample_value,
+            title = "Populasi (Ribu Jiwa)", 
+            opacity = 0.5, na.label = "")
+
+# dusun -----
+dusun = readOGR("randompolygon_dusun/diy/bantul/pajangan-guwosari.geojson")
+
+## Leaflet
+pal = colorNumeric("viridis", dusun$value)
+
+leaflet(dusun) %>%
+  addTiles() %>%
+  addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = 0.7,
+              fillColor = ~pal(value),
+              label = ~paste0(pedukuhan, ": ", prettyNum(value, ","))) %>%
+  addLegend("topright", pal = pal, values = ~value,
             title = "Populasi (Ribu Jiwa)", 
             opacity = 0.5, na.label = "")
